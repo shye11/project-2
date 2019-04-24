f4bffc0d3cbb41470d58fbb8b67cfae63dc52b5b
Michael Rosario <mjfrosario@gmail.com>
2019-04-23 23:50:43 -0400

    add two navigational options

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,3 +1,21 @@
 $(function() {
- 
+    $(".hamburger").on("click",function(e){
+        e.preventDefault();
+        var currentNav = $(this);
+        if(currentNav.hasClass("open")){
+            currentNav
+                .removeClass("open")
+                .parent().find("ul").removeClass("open");
+        } else {
+            currentNav
+                .addClass("open")
+                .parent().find("ul").addClass("open");
+        }
+    });
+
+    $("nav").on('mouseleave',function(){
+
+        $(".hamburger, nav ul").removeClass("open")
+
+    });
 });
