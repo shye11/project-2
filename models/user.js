1569bf9a29844945f7952f94b9c66c1d84d95fd0
Shy <shyloh.elder@gmail.com>
2019-04-29 19:59:19 -0400

    updating passport

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      required: true,
       validate: {
         len: [1]
       }
