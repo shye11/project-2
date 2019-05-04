dc2e3a35284447773888e2d9a4294ca9dab7a0e0
Michael Rosario <mjfrosario@gmail.com>
2019-05-03 21:56:16 -0400

    update carousel with DB and handlebars

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -102,4 +102,23 @@ $(function() {
     
     });
 
+    $(".carousel.carousel-option-one").slick({
+        lazyLoad: 'ondemand',
+        dots: false,
+        slidesToShow: 1,
+        centerMode: false,
+    });
+    $(".carousel.carousel-option-two").slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $(".carousel.carousel-option-three").slick({
+        lazyLoad: 'ondemand',
+        dots: true,
+        slidesToShow: 1,
+        centerMode: true,
+    });
+
 });
